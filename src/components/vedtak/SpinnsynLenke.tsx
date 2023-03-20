4817a6afdc16fb53b03cfbc6af22ff830e594636
Anders Rognstad <anders.rognstad@nav.no>
2023-03-20 10:38:02 +0100

    Fix prettier

--- src/components/vedtak/SpinnsynLenke.tsx
+++ src/components/vedtak/SpinnsynLenke.tsx
@@ -1,22 +1,22 @@
 import Lenke from "nav-frontend-lenker";
 import React from "react";
-import {erLokal, erPreProd, fullNaisUrlIntern} from "@/utils/miljoUtil";
-import {useNavBrukerData} from "@/data/navbruker/navbruker_hooks";
-import {ExternalLink} from "@navikt/ds-icons";
+import { erLokal, erPreProd, fullNaisUrlIntern } from "@/utils/miljoUtil";
+import { useNavBrukerData } from "@/data/navbruker/navbruker_hooks";
+import { ExternalLink } from "@navikt/ds-icons";
 
 export const spinnsynUrl = function () {
-    const path = "/syk/sykepenger"
-    if (erLokal()) {
-        return path;
-    }
-    if (erPreProd()) {
-        `https://spinnsyn-frontend-interne.intern.dev.nav.no${path}`;
-    }
-    return fullNaisUrlIntern("spinnsyn-frontend-interne", path);
+  const path = "/syk/sykepenger";
+  if (erLokal()) {
+    return path;
+  }
+  if (erPreProd()) {
+    `https://spinnsyn-frontend-interne.intern.dev.nav.no${path}`;
+  }
+  return fullNaisUrlIntern("spinnsyn-frontend-interne", path);
 };
 
 export const SpinnsynLenke = () => {
-    const navbruker = useNavBrukerData();
+  const navbruker = useNavBrukerData();
 
   return (
     <Lenke href={spinnsynUrl()} target="_blank" rel="noopener noreferrer">
