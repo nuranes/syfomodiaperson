41a6c65f675794a23011bc6fe1ddac7dd3e8636e
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2023-03-20 10:35:06 +0100

    Nye flex ingresser dev (#874)

--- src/components/vedtak/SpinnsynLenke.tsx
+++ src/components/vedtak/SpinnsynLenke.tsx
@@ -1,14 +1,22 @@
 import Lenke from "nav-frontend-lenker";
 import React from "react";
-import { fullNaisUrlIntern } from "@/utils/miljoUtil";
-import { useNavBrukerData } from "@/data/navbruker/navbruker_hooks";
-import { ExternalLink } from "@navikt/ds-icons";
+import {erLokal, erPreProd, fullNaisUrlIntern} from "@/utils/miljoUtil";
+import {useNavBrukerData} from "@/data/navbruker/navbruker_hooks";
+import {ExternalLink} from "@navikt/ds-icons";
 
-export const spinnsynUrl = () =>
-  fullNaisUrlIntern("spinnsyn-frontend-interne", "/syk/sykepenger");
+export const spinnsynUrl = function () {
+    const path = "/syk/sykepenger"
+    if (erLokal()) {
+        return path;
+    }
+    if (erPreProd()) {
+        `https://spinnsyn-frontend-interne.intern.dev.nav.no${path}`;
+    }
+    return fullNaisUrlIntern("spinnsyn-frontend-interne", path);
+};
 
 export const SpinnsynLenke = () => {
-  const navbruker = useNavBrukerData();
+    const navbruker = useNavBrukerData();
 
   return (
     <Lenke href={spinnsynUrl()} target="_blank" rel="noopener noreferrer">
