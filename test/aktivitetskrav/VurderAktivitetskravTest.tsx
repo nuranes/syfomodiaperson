7e32e2a243b1002a926d644d739d28029bc325de
Solgård <vetle.bu.solgard@nav.no>
2023-10-20 14:51:46 +0200

    IS-1675: Update test. Ikke oppfylt button should not show when forhandsvarsel is sent. Only when it is expired

--- test/aktivitetskrav/VurderAktivitetskravTest.tsx
+++ test/aktivitetskrav/VurderAktivitetskravTest.tsx
@@ -295,7 +295,8 @@ describe("VurderAktivitetskrav", () => {
       expect(screen.queryByRole("button", { name: "Sett unntak" })).to.exist;
       expect(screen.queryByRole("button", { name: "Er i aktivitet" })).to.exist;
       expect(screen.queryByRole("button", { name: "Ikke aktuell" })).to.exist;
-      expect(screen.queryByRole("button", { name: "Ikke oppfylt" })).to.exist;
+      expect(screen.queryByRole("button", { name: "Ikke oppfylt" })).to.not
+        .exist;
       expect(screen.queryByRole("button", { name: "Avvent" })).to.not.exist;
     });
 
